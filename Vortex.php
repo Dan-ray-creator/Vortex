8f39bef78caf75f05da70ae4729900bdbaaa2615
Dan-ray-creator <it.danraycguevarra@gmail.com>
2021-02-04 00:15:17 +0800

    Vortex

---
+++ Vortex.php
@@ -0,0 +1,10 @@
+<?php
+ echo "Git 101!";
+echo "Vortex";
+echo "Members:";
+echo "Dan Ray Guevarra";
+echo "Alvin Canlas";
+echo "Charles Celso";
+echo "Ram Rainier Belleza";
+echo "JPatrick Joseph Canlas";
+?> 
